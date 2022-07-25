6f288ba695398b7ea972318ad912817245ea5bad
cbermedo <cristian@izytech.cl>
2022-07-25 07:30:25 -0400

    first commit

---
+++ src/GiftExpertApp.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useState } from 'react';
+import { AddCategory, GifGrid} from './components';
+
+export const GiftExpertApp = () => {
+  const [categories, setCategories] = useState(['one punch']);
+
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+
+    setCategories([newCategory, ...categories]);
+  };
+
+  return (
+    <>
+      <h1>GiftExpertApp</h1>
+
+      <AddCategory onNewCategory={onAddCategory} />
+
+      {categories.map((category) => (
+        <GifGrid 
+          key={category} 
+          category={category} />
+      ))}
+    </>
+  );
+};
